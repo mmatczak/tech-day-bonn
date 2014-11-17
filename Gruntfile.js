35820687ab35da5c8d1015ab81d1951192d9d5d6
Marek Matczak <marek.matczak@capgemini.com>
2014-11-17 20:07:01 +0100

    from_scratch initially added

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost'
+            },
+            develop: {
+                options: {
+                    port: 9000,
+                    open: {
+                        target: 'http://localhost:9000/'
+                    },
+                    base: ['.tmp', 'app'],
+                    middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+                        var cacheClear = function (req, res, next) {
+                            res.setHeader('Expires', 'Thu, 01 Jan 1970 00:00:00 GMT');
+                            res.setHeader('Pragma', 'no-cache');
+                            res.setHeader('Cache-Control', 'no-store');
+                            next();
+                        }, middlewares = [cacheClear], directory = (options.directory || options.base[options.base.length - 1]);
+                        // Serve static files.
+                        options.base.forEach(function (base) {
+                            middlewares.push(connect.static(base));
+                        });
+                        // Make directory browse-able.
+                        middlewares.push(connect.directory(directory));
+
+                        return middlewares;
+                    }
+                }
+            }
+        },
+        less: {
+            all: {
+                options: {
+                    compress: false
+                },
+                files: {
+                    '.tmp/main/css/main.css': 'app/main/css/main.less'
+                }
+            }
+        },
+        wiredep: {
+            develop: {
+                src: ['app/index.html'],
+                ignorePath: new RegExp('^app/')
+            }
+        },
+        watch: {
+            options: { livereload: true },
+            less: {
+                files: ['app/main/**/*.less'],
+                tasks: ['less']
+            },
+            all: {
+                files: ['app/**', '!/app/bower_components/**']
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: false
+            },
+            unit_chrome: {
+                configFile: 'karma.conf.js',
+                singleRun: false,
+                browsers: [
+                    'Chrome'
+                ]
+            },
+            ci: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        },
+        coveralls: {
+            options: {
+                debug: true,
+                coverage_dir: 'coverage/',
+                dryRun: false,
+                force: true,
+                recursive: true
+            }
+        }
+    });
+
+    grunt.registerTask('test', [
+        'karma:unit'
+    ]);
+    grunt.registerTask('test:debug', [
+        'karma:unit_chrome'
+    ]);
+    grunt.registerTask('test:ci', [
+        'karma:ci', 'coveralls'
+    ]);
+    grunt.registerTask('default', [
+        'karma:unit'
+    ]);
+    grunt.registerTask('serve', [
+        'less', 'wiredep', 'connect:develop', 'watch'
+    ]);
+    grunt.registerTask('serve-minimal', [
+        'connect:develop', 'watch'
+    ]);
+};
